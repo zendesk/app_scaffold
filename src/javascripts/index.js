525e438beb04f7a9a009419153f0ae6280814c98
Daniel Ribeiro <djbreves@gmail.com>
2016-07-28 17:03:25 +1000

    Refactor for reusability

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,8 +1,12 @@
-import LegacyApp from './legacy_app';
 import ZAFClient from 'zendesk_app_framework_sdk';
+import I18n from 'i18n';
+import LegacyApp from './legacy_app';
 
 var client = ZAFClient.init();
 
-client.on('app.registered', function(data) {
-  new LegacyApp(client, data);
+client.on('app.registered', function(appData) {
+  client.get('currentUser.locale').then(userData => {
+    I18n.loadTranslations(userData['currentUser.locale']);
+    new LegacyApp(client, appData);
+  });
 });
