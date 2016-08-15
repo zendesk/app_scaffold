5745dfdf281a6b4c933cdb2c0fcce6c383eacfae
Ed Pritchard <pritchardpro@gmail.com>
2016-08-15 16:33:57 -0500

    Switch to https for external assets so browsers don't cry about it

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ var externalAssets = {
     'https://cdn.jsdelivr.net/bootstrap/2.3.2/css/bootstrap.min.css'
   ],
   js: [
-    'http://cdn.jsdelivr.net/g/lodash@2.4.2(lodash.underscore.min.js),handlebarsjs@1.3.0,jquery@2.2.4,momentjs@2.9.0,bootstrap@2.3.2',
-    'http://assets.zendesk.com/apps/sdk/2.0/zaf_sdk.js'
+    'https://cdn.jsdelivr.net/g/lodash@2.4.2(lodash.underscore.min.js),handlebarsjs@1.3.0,jquery@2.2.4,momentjs@2.9.0,bootstrap@2.3.2',
+    'https://assets.zendesk.com/apps/sdk/2.0/zaf_sdk.js'
   ]
 }
 
