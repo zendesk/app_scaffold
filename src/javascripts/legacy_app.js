29306cab6e5857d92596f27643c2119cdae20ffd
Roman Sandler <rsandler@zendesk.com>
2016-10-20 15:52:33 +1100

    Revert "Merge pull request #31 from zendesk/roman/modal"
    This reverts commit 6f79b47bcc484982ca00fd85b864e122db3f8757, reversing
    changes made to 3d74d7e59130ec6ac92067f6be081d3de854cf41.

--- src/javascripts/legacy_app.js
+++ src/javascripts/legacy_app.js
@@ -17,31 +17,15 @@ var App = {
   events: {
     'app.created': 'init',
     'getMe.done': 'renderMain',
-    'click .js-modalButton': 'showModal',
-    'modal.close': 'logClosedModal',
     'app.willDestroy': 'logClosedApp'
   },
 
   init: function() {
-    if (this.currentLocation() === 'modal') {
-      return this.switchTo('modal');
-    }
     this.ajax('getMe');
   },
 
   renderMain: function(data) {
     this.switchTo('main', data.user);
-  },
-
-  showModal: function() {
-    this.modal(window.location.href).then(function() {
-      this.modalCount = (this.modalCount || 0) + 1;
-      console.log('Modals shown: ' + this.modalCount);
-    }.bind(this));
-  },
-
-  logClosedModal: function() {
-    console.log('Closed a modal.');
   },
 
   logClosedApp: function() {
