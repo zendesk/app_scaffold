4fadf13225509aafa12002ccd0d0582ec7f76bec
Lisa Friedman <lfriedman@zendesk.com>
2017-07-26 17:20:33 -0700

    adding probability and translation to the app

--- src/javascripts/ticket_sidebar.js
+++ src/javascripts/ticket_sidebar.js
@@ -24,13 +24,42 @@ class TicketSidebar {
 
   getLanguages() {
     return this.client.get(['ticket.requester.locale', 'ticket.description']).then(function(data) {
-      data["ticket.description.language"] = I18n.t(Franc(data["ticket.description"]));
+      var description = data["ticket.description"];
       data["ticket.requester.language"] = I18n.t(data["ticket.requester.locale"]);
+
+      if (description == undefined) {
+        data["ticket.description.language"] = "Empty ticket description"
+      } else {
+        data["ticket.description.language"] = I18n.t(Franc(description));
+        data["ticket.language.probability"] = Franc.all(data["ticket.description"])[0][1];
+      }
       return data
     });
   }
 
+  setCustomField(data) {
+    var language = data["ticket.description.language"];
+    var languages = {
+      "English": "english",
+      "Spanish": "spanish",
+      "Russian": "russian",
+      "German": "german",
+      "French": "french",
+      "Dutch": "dutch",
+      "Italian": "italian",
+      "Portuguese": "portuguese",
+      "Japanese": "japanese",
+      "Chinese": "chinese"
+    }
+    if (languages[language]) {
+      return this.client.set('ticket.customField:custom_field_114095962393', languages[language]);
+    } else {
+      console.log("no language");
+    }
+  }
+
   renderMain(data) {
+    this.setCustomField(data);
     this.view.switchTo('main', data);
   }
 }
