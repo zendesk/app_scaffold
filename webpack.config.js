6a4f6802576ce26f72cf2fa5be2747103436825d
Tony Tian <ttian@zendesk.com>
2018-08-07 14:55:02 +1000

    - generate jsDelivr url for Garden assets - utilise html-webpack-plugin to dynamically generate iframe.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,31 @@ const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const devDependencies = require('./package.json').devDependencies
 const TranslationsPlugin = require('./webpack/translations-plugin')
+
+// this function reads Zendesk Garden npm dependencies from package.json and
+// creates a jsDelivr url
+const zendeskGardenJsDelivrUrl = (function() {
+  const pkg = Object.keys(devDependencies).filter(item => item.includes('@zendeskgarden/css'));
+
+  return pkg.reduce((url, pkg) => {
+    const version = devDependencies[pkg]
+      .replace(/^[\^~]/g,'')
+      .replace(/\.\d$/,'');
+    return url = `${url}npm/${pkg}@${version},`
+  }, "https://cdn.jsdelivr.net/combine/").slice(0, -1);
+}());
+
+const externalAssets = {
+  css: [
+    zendeskGardenJsDelivrUrl
+  ],
+  js: [
+    'https://assets.zendesk.com/apps/sdk/2.0/zaf_sdk.js'
+  ]
+};
 
 module.exports = {
   entry: {
@@ -34,7 +58,7 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          {loader: 'css-loader', options: { url: false }},
+          {loader: 'css-loader', options: {url: false}},
           'postcss-loader'
         ]
       }
@@ -48,8 +72,7 @@ module.exports = {
     // Copy over static assets
     new CopyWebpackPlugin([
       { from: 'src/manifest.json', to: '../', flatten: true },
-      { from: 'src/images/*', to: '.', flatten: true },
-      { from: 'src/templates/iframe.html', to: '.', flatten: true }
+      { from: 'src/images/*', to: '.', flatten: true }
     ]),
 
     new MiniCssExtractPlugin({
@@ -58,6 +81,14 @@ module.exports = {
 
     new TranslationsPlugin({
       path: path.resolve(__dirname, './src/translations')
+    }),
+
+    new HtmlWebpackPlugin({
+      warning: 'AUTOMATICALLY GENERATED FROM ./src/templates/iframe.html - DO NOT MODIFY THIS FILE DIRECTLY',
+      vendorCss: externalAssets.css,
+      vendorJs: externalAssets.js,
+      template: './src/templates/iframe.html',
+      filename: 'iframe.html'
     })
   ]
 }
